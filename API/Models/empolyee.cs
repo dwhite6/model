8dcc81bb7f08a6ccf82efac55b8dc08ad36a0451
Deonante White <deonante@Deonantes-MacBook-Air.local>
2020-11-15 12:35:53 -0600

    test

--- API/Models/empolyee.cs
+++ API/Models/empolyee.cs
@@ -10,7 +10,7 @@ namespace mis321_project.API.Models
         public int id {get; set;}
         public string empolyeeFName {get; set;}
         public string empolyeeLName {get; set;}
-        public int empolyeeStatus {get; set;}
+        public string empolyeeStatus {get; set;}
         public int overallScore {get; set;}
         public int performanceScore {get;set;}
         public int leadershipScore {get;set;}
