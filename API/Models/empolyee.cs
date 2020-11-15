05008213a93356d14638ea47a50538657e9bb9e8
Deonante White <deonante@Deonantes-MacBook-Air.local>
2020-11-15 11:46:34 -0600

    test

---
+++ API/Models/empolyee.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using API.Models.Interfaces;
+
+namespace mis321_project.API.Models
+{
+    public class empolyee
+    {
+        public int id {get; set;}
+        public string empolyeeFName {get; set;}
+        public string empolyeeLName {get; set;}
+        public int empolyeeScore {get;set;}
+    }
+}
